fe3936f91e6110419e1f89da796be888c44acdc3
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-06 23:09:17 +0800

    [TodoList][Alisa & Fennie] test: should_return_todoitem_id_1_when_add_todoitem_given_todoitem

--- src/test/java/com/thoughtworks/springboottodolist/service/TodoServiceImplTest.java
+++ src/test/java/com/thoughtworks/springboottodolist/service/TodoServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springboottodolist.service;
 
+import com.thoughtworks.springboottodolist.dto.RequestTodo;
 import com.thoughtworks.springboottodolist.entity.TodoItem;
 import com.thoughtworks.springboottodolist.respository.TodoItemRespository;
 import org.junit.jupiter.api.Assertions;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,10 +30,10 @@ class TodoServiceImplTest {
     void should_retun_size_2_when_todolist_have_2_item() {
         //given
         List<TodoItem> todoItemList = new ArrayList<>();
-        for (int i = 1; i<=2; i++) {
+        for (int i = 1; i <= 2; i++) {
             TodoItem todoItem = new TodoItem();
             todoItem.setId(i);
-            todoItem.setContent(i+ "content");
+            todoItem.setContent(i + "content");
             todoItem.setStatus("false");
             todoItemList.add(todoItem);
         }
@@ -45,7 +47,23 @@ class TodoServiceImplTest {
     }
 
     @Test
-    void addTodoItem() {
+    void should_return_todoitem_id_1_when_add_todoitem_given_todoitem() {
+        //given
+        TodoItem todoItem = new TodoItem();
+        todoItem.setId(1);
+        todoItem.setContent("content");
+        todoItem.setStatus("false");
+        RequestTodo requestTodo = new RequestTodo();
+        requestTodo.setContent("content");
+        requestTodo.setStatus(false);
+
+        when(todoItemRespository.save(any())).thenReturn(todoItem);
+
+        //when
+        String result = todoService.addTodoItem(requestTodo).getId();
+
+        //then
+        Assertions.assertEquals("1", result);
     }
 
     @Test
